95420f58c41630adc68052dbcf4952085905244b
Cecilia Chan <cece@vl965-172-31-150-221.wireless.umass.edu>
2022-09-27 19:59:25 -0400

    Added Post and Get comment, as well as json storage files for comments

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import logger from 'morgan';
-import * as db from './db.js';
 import * as fs from 'fs';
 import {v4 as uuidv4} from 'uuid';
 
@@ -11,11 +10,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//const userJSON = 'users.json';
 let userJSON = { table: [] };
-//const postsJSON = 'posts.json';
 let postsJSON = { table: [] };
-const commentsJSON = 'comments.json';
+let commentsJSON = { table: [] };
 
 app.post('/api/user/create', (req, res) => {
     const { name } = req.body;
@@ -54,21 +51,15 @@ app.post('/api/posts/create', (req, res) => {
     const { userid, content } = req.body;
     let postId = uuidv4();
   
-    if (content === undefined) {
+    if (content === undefined || userid === undefined) {
       res.status(400).json({
-        message: 'Missing content'
+        message: 'Missing content and/or userid'
       });
       return;
     }
     else if (content.length > 128){
         res.status(400).json({
             message: 'Name exceeds 128 characters'
-        });
-        return;
-    }
-    else if (userid === undefined){
-        res.status(404).json({
-            message: 'User does not exist'
         });
         return;
     }
@@ -83,9 +74,8 @@ app.post('/api/posts/create', (req, res) => {
         });
         return;
       }
-      post = { postid: postId, userid, name: user.name, content };
+      post = { postid: postId, userid: userid, name: user.name, content: content };
       fs.readFile('posts.json', 'utf8', function(err, data2){
-        //console.log(data); 
         let postContent = JSON.parse(data2);
         postContent.table.push(post);
         postsJSON = postContent;
@@ -97,8 +87,83 @@ app.post('/api/posts/create', (req, res) => {
       res.json(post);
       return;
     });
-    //const post = db.savePost(userid, content);
-    //res.json(post);
+});
+
+app.post('/api/comments/create', (req, res) => {
+  const { userid, postid, content } = req.body;
+  let commentId = uuidv4();
+
+  if (content === undefined || userid === undefined || postid === undefined) {
+    res.status(400).json({
+      message: 'Missing content, userid, and/or postid'
+    });
+    return;
+  }
+  else if (content.length > 128){
+      res.status(400).json({
+          message: 'Name exceeds 128 characters'
+      });
+      return;
+  }
+
+  let comment = {};
+  fs.readFile('users.json', 'utf8', function(err, data1){
+    let userContent = JSON.parse(data1);
+    const user = userContent.table.find((user) => user.userid == userid);
+    if (user === undefined){
+      res.status(404).json({
+        message: 'User does not exist'
+      });
+      return;
+    }
+    fs.readFile('posts.json', 'utf8', function(err, data2){
+      let postContent = JSON.parse(data2);
+      const posts = postContent.table.find((post) => post.postid == postid);
+      if (posts === undefined){
+        res.status(404).json({
+          message: 'Post does not exist'
+        });
+        return;
+      }
+      comment = { commentid: commentId, postid: postid, userid: userid, name: posts.name, content: content };
+      fs.readFile('comments.json', 'utf8', function(err, data3){
+        let commentContent = JSON.parse(data3);
+        commentContent.table.push(comment);
+        commentsJSON = commentContent;
+        fs.writeFile('comments.json', JSON.stringify(commentsJSON), function(err) {
+          if (err) throw err;
+          console.log('complete');
+        });
+        res.json(comment);
+        return;
+      });
+    });
+  });
+});
+
+app.get('/api/comments/get', (req, res) => {
+  const { commentid } = req.body;
+
+  if (commentid === undefined){
+    res.status(400).json({
+      message: 'Missing commentid'
+    });
+    return;
+  }
+  let getComment = {};
+  fs.readFile('comments.json', 'utf8', function(err, data1){
+    let commentContent = JSON.parse(data1);
+    const comment = commentContent.table.find((comment) => comment.commentid == commentid);
+    if ( comment === undefined){
+      res.status(404).json({
+        message: 'Comment does not exist'
+      });
+      return;
+    }
+    getComment = { commentid: commentid, postid: comment.postid, userid: comment.userid, name: comment.name, content: comment.content };
+    res.json(getComment);
+    return;
+  });
 });
 
 app.listen(port, () => {
