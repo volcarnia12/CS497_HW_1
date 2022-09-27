17274e79b609eec4d4c9c8a9d7a229362edf41cf
Cecilia Chan <cece@vl965-172-31-150-221.wireless.umass.edu>
2022-09-27 17:38:28 -0400

    Completed User and Post creation, along with json file storage for each

--- db.js
+++ db.js
@@ -1,15 +1,54 @@
-const users = [];
-const comments = [];
+import * as fs from 'fs';
+
+//const users = [];
+//const posts = [];
 
 let userId = 0;
-let commentId = 0;
+let postId = 0;
+
+let users = [];
+let posts = [];
+let comments = [];
+
+const JSONfile = 'users.json';
+const JSONfile2 = 'posts.json';
+const JSONfile3 = 'comments.json';
+
+//const fs = require('fs');
 
 export const saveUser = (name) => {
-  let user = { id: userId, name };
+  console.log(userId);
+  
+  //const users = JSON.parse(JSONfile);
+  /*if (undefined === JSON.parse(JSONfile)){
+    user = { userid: 0, name };
+  }
+  else{
+    userId = JSON.parse(JSONfile).length + 1;
+    user = { userid: userId, name};
+  }*/
+  let user = { userid: userId, name };
   users[userId] = user;
   userId++;
+  console.log(users);
   return user;
 };
+
+export const savePost = (userid, content) => {
+  console.log(users);
+  let comment = { postid: postId, userid, name: users[userid], content };
+  //console.log(users[userid]);
+  if (users[userid] === undefined) {
+    return undefined;
+  }
+
+  posts[postId] = comment;
+  console.log(posts);
+  postId++;
+  return comment;
+};
+
+////
 
 export const getPost = (postId) => {
   if (posts[postId] === undefined) {
@@ -21,18 +60,6 @@ export const getPost = (postId) => {
 
 export const getPosts = () => {
   return posts;
-};
-
-export const saveComment = (postId, body) => {
-  let comment = { id: commentId, postId, body };
-
-  if (posts[postId] === undefined) {
-    return null;
-  }
-
-  comments[commentId] = comment;
-  commentId++;
-  return comment;
 };
 
 export const getComment = (commentId) => {
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import logger from 'morgan';
 import * as db from './db.js';
+import * as fs from 'fs';
+import {v4 as uuidv4} from 'uuid';
 
 const app = express();
 const port = 3000;
@@ -9,8 +11,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//const userJSON = 'users.json';
+let userJSON = { table: [] };
+//const postsJSON = 'posts.json';
+let postsJSON = { table: [] };
+const commentsJSON = 'comments.json';
+
 app.post('/api/user/create', (req, res) => {
     const { name } = req.body;
+    let content = "";
+    let userId = uuidv4();
   
     if (name === undefined) {
       res.status(400).json({
@@ -25,8 +35,70 @@ app.post('/api/user/create', (req, res) => {
         return;
     }
 
-    const post = db.saveUser(name);
-    res.json(post);
+    let user = { userid: userId, name};
+    fs.readFile('users.json', 'utf8', function(err, data){
+      console.log(data);
+      content = JSON.parse(data);
+      content.table.push(user);
+      userJSON = content;
+      fs.writeFile('users.json', JSON.stringify(userJSON), function(err) {
+        if (err) throw err;
+        console.log('complete');
+        });
+    });
+
+    res.json(user);
+});
+
+app.post('/api/posts/create', (req, res) => {
+    const { userid, content } = req.body;
+    let postId = uuidv4();
+  
+    if (content === undefined) {
+      res.status(400).json({
+        message: 'Missing content'
+      });
+      return;
+    }
+    else if (content.length > 128){
+        res.status(400).json({
+            message: 'Name exceeds 128 characters'
+        });
+        return;
+    }
+    else if (userid === undefined){
+        res.status(404).json({
+            message: 'User does not exist'
+        });
+        return;
+    }
+    let post = {};
+
+    fs.readFile('users.json', 'utf8', function(err, data1){
+      let contentPost = JSON.parse(data1);
+      const user = contentPost.table.find((user) => user.userid == userid);
+      if (user === undefined){
+        res.status(404).json({
+          message: 'User does not exist'
+        });
+        return;
+      }
+      post = { postid: postId, userid, name: user.name, content };
+      fs.readFile('posts.json', 'utf8', function(err, data2){
+        //console.log(data); 
+        let postContent = JSON.parse(data2);
+        postContent.table.push(post);
+        postsJSON = postContent;
+        fs.writeFile('posts.json', JSON.stringify(postsJSON), function(err) {
+          if (err) throw err;
+          console.log('complete');
+        });
+      });
+      res.json(post);
+      return;
+    });
+    //const post = db.savePost(userid, content);
+    //res.json(post);
 });
 
 app.listen(port, () => {
